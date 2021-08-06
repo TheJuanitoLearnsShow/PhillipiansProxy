69649273783450cbcea5fd171cf80db6cbc87cc7
Juan Tarquino <jptarqu@gmail.com>
2021-08-06 18:27:35 -0400

    python attempt

---
+++ PhillipiansProxy/TensorModel/KerasTest.cs
@@ -0,0 +1,23 @@
+﻿using Keras.Datasets;
+using Keras.Layers;
+using Keras.Models;
+using Keras.Utils;
+using Numpy;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace TensorModel
+{
+    class KerasTest
+    {
+        object load_model(string model_path)
+        {
+           
+            var model = tf.keras.models.load_model(model_path, custom_objects ={
+                        'KerasLayer': hub.KerasLayer
+            return model
+        }
+    
+}
+}
